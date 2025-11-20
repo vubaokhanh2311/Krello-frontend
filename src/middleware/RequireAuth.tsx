e65c31cb087e4617909457af462d944ec4a5106b
VuBaoKhanh <153911764+vubaokhannh@users.noreply.github.com>
2025-11-20 10:08:53 +0700

    feat: Add login middleware (RequireAuth), update router add page notfound

---
+++ src/middleware/RequireAuth.tsx
@@ -0,0 +1,33 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { ReactNode, useEffect, useState } from "react";
+import { notifications } from "@mantine/notifications";
+
+interface Props {
+  children: ReactNode;
+}
+
+const RequireAuth = ({ children }: Props) => {
+  const token = localStorage.getItem("accessToken");
+  const location = useLocation();
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    if (!token && !showNotification) {
+      notifications.show({
+        title: "Cần đăng nhập",
+        message: "Vui lòng đăng nhập để truy cập trang này!",
+        color: "yellow",
+        autoClose: 3000,
+      });
+      setShowNotification(true);
+    }
+  }, [token, showNotification]);
+
+  if (!token) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <>{children}</>;
+};
+
+export default RequireAuth;

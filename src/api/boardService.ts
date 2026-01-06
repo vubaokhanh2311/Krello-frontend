2f83166f2d3b60e878dd93f7997df7cacf2ec851
VuBaoKhanh <153911764+vubaokhannh@users.noreply.github.com>
2026-01-06 10:36:09 +0700

    update boardService

--- src/api/boardService.ts
+++ src/api/boardService.ts
@@ -2,29 +2,39 @@ import RestClient from "./RestClient";
 import type { ApiResponse } from "../types/ApiResponse";
 import type { BoardQuery, Board, BoardRequest } from "../types/BoardType";
 
-export async function getBoard(query?: BoardQuery) {
-  return RestClient.get("/boards", {
+export async function getBoard(
+  query?: BoardQuery
+): Promise<ApiResponse<Board[]>> {
+  return RestClient.get<ApiResponse<Board[]>>("/boards", {
     params: query,
   });
 }
 
-export async function getBoardsJoinedByUser(query?: BoardQuery) {
-  return RestClient.get("/boards/joined", {
+export async function getBoardsJoinedByUser(
+  query?: BoardQuery
+): Promise<ApiResponse<Board[]>> {
+  return RestClient.get<ApiResponse<Board[]>>("/boards/joined", {
     params: query,
   });
 }
 
-export async function CreateBoard(values: BoardRequest) {
-  const res = await RestClient.post("/boards", values);
+export async function CreateBoard(
+  values: BoardRequest
+): Promise<ApiResponse<Board>> {
+  const res = await RestClient.post<ApiResponse<Board>>("/boards", values);
   return res;
 }
 
-export async function getBoardDetail(boardId: string) {
-  const res = await RestClient.get(`/boards/${boardId}`);
+export async function getBoardDetail(
+  boardId: string
+): Promise<ApiResponse<Board>> {
+  const res = await RestClient.get<ApiResponse<Board>>(`/boards/${boardId}`);
   return res;
 }
 
-export async function searchBoards(keyword: string) {
+export async function searchBoards(
+  keyword: string
+): Promise<ApiResponse<Board[]>> {
   return RestClient.get<ApiResponse<Board[]>>("/boards", {
     params: {
       page: 1,
